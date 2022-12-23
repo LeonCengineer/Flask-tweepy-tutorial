fc5f5fdc32ccd12964a5510cc6097e6c9eebd0bc
LeonCengineer <leonhcopeland@gmail.com>
2022-12-23 04:33:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	
-	
 	auth = tweepy.OAuth1UserHandler(
 	   '7UzudjdAzuBxfRBDI8yo0KhXh', 'gXcSE0AYYDSZDOUaAtEgUDYJ9QCV1ifXgLLEM6EOUDjbZrmXQA', '2638191615-S6kii5fXiPPdSqJrasLYxrEVnJZRfhMaEj307y0', 'U3VrNfUoNkscC8DkYJhRpYVdt9WcYYzUCOBgd9sFIFy3B')
 	)
